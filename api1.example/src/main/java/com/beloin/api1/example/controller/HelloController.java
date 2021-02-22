37b8641ac48c45f80792e9f539a7d59f2bdb4d34
Beloin Sena <sena@pop-os.localdomain>
2021-02-21 22:02:17 -0300

    Gateway Example

---
+++ api1.example/src/main/java/com/beloin/api1/example/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.beloin.api1.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World 1";
+    }
+}
---
+++ gateway.example/src/main/java/com/beloin/gateway/example/Application.java
@@ -0,0 +1,15 @@
+package com.beloin.gateway.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
